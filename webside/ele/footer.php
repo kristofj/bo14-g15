8ad71f546288f09cc47c2c6a95a56df827820977
kristofj <kristoffer.jensen@hiof.no>
2014-01-07 18:02:20 +0100

    lagt til innhold

--- webside/ele/footer.php
+++ webside/ele/footer.php
@@ -1,5 +1,5 @@
 <footer>
 	<p class="footer">
-		<a class="footer" href=""></a>
+		<a class="footer" href="http://www.hiof.no/nor/hogskolen-i-ostfold/for-studenter/studieplaner/2011-_-2012/informasjonsteknologi/?&displayitem=5447&module=studieinfo&type=emne">Bacheloroppgave</a> ved <a class="footer" href="http://www.hiof.no">Høgskolen i Østfold</a>, våren 2014.
 	</p>
 </footer>
