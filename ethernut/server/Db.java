309c6edc34f8dbfad6cdae8df630efcfd2ce01f9
Leakim1991 <mikael-91@hotmail.com>
2014-02-28 00:30:15 +0100

    parse fra json og dytte inn i DB er ferdig

--- ethernut/server/Db.java
+++ ethernut/server/Db.java
@@ -2,8 +2,6 @@ import java.sql.*;
 import java.util.Properties;
 
 public class Db {
-	String sql;
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://j3nsen.com/hessdalen_weather";  
 	static final String USER = "hessdalen";
 	static final String PASS = "hessdalen123";
@@ -13,29 +11,20 @@ public class Db {
 		Connection conn = null;
 		Statement stmt = null;
 		try{
-			//STEP 2: Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
-
-			//STEP 3: Open a connection
-			System.out.println("Connecting to database...");
 			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-			//STEP 4: Execute a query
 			stmt = conn.prepareStatement(sql);
 			stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-			ResultSet rs = stmt.getGeneratedKeys();	
-			int autoIncKeyFromApi = -1;
+			ResultSet rs = stmt.getGeneratedKeys();
 			rs = stmt.getGeneratedKeys();
 			if (rs.next()) {
 				key = rs.getInt(1);
-				System.out.println(key);
 			} else {
-				// throw an exception from here
 			}
-			//STEP 6: Clean-up environment
 			rs.close();
 			stmt.close();
 			conn.close();
+            System.out.println("Executed: "+sql);
 			}catch(SQLException se){
 			se.printStackTrace();
 			}catch(Exception e){
--- ethernut/server/Hello.java
+++ ethernut/server/Hello.java
@@ -1,60 +1,109 @@
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
 public class Hello {
-//Main
-String entryKey;
-Datetime mainDatetime;
+    static int entryKey;
+    static String mainDatetime;
+    static JSONObject json;
+    static String jsonString = "{" +
+            "     \"mainDatetime\": \"1991-08-23 22:55:45\"," +
+            "     \"temp\":" +
+            "     {" +
+            "         \"avg\": 1.2," +
+            "         \"now\": 23.2," +
+            "         \"max\": 1.2," +
+            "         \"timeMax\": \"1991-08-23 22:55:45\"," +
+            "         \"min\": 1.2," +
+            "         \"timeMin\": \"1991-08-23 22:55:45\"" +
+            "     }," +
+            "     \"humidity\":" +
+            "     {" +
+            "         \"avg\": 1.2," +
+            "         \"now\": 1.2," +
+            "         \"max\": 1.2," +
+            "         \"timeMax\": \"1991-08-23 22:55:45\"," +
+            "         \"min\": 1.2," +
+            "         \"timeMin\": \"1991-08-23 22:55:45\"" +
+            "     }," +
+            "     \"pressure\":" +
+            "     {" +
+            "         \"avg\": 1.2," +
+            "         \"now\": 1.2," +
+            "         \"max\": 1.2," +
+            "         \"timeMax\": \"1991-08-23 22:55:45\"," +
+            "         \"min\": 1.2," +
+            "         \"timeMin\": \"1991-08-23 22:55:45\"" +
+            "     }," +
+            "     \"wind\":" +
+            "     {" +
+            "         \"avg\": 1.2," +
+            "         \"now\": 1.2," +
+            "         \"max\": 1.2," +
+            "         \"timeMax\": \"1991-08-23 22:55:45\"," +
+            "         \"maxDir\" : 1.2," +
+            "         \"min\": 1.2," +
+            "         \"timeMin\": \"1991-08-23 22:55:45\"" +
+            "     }" +
+            " }";
 
-//Temperature
-Double tempAvg;
-Double tempNow;
-Double tempMax;
-Datetime tempMaxDatetime;
-Double tempMin;
-Datetime tempMinDatetime;
-
-//Wind
-Double windAvg;
-Double windNow;
-Double windMax;
-Double windMaxDir;
-Datetime windMaxDatetime;
-Double windMin;
-Datetime windMinDatetime;
-
-//Pressure
-Double pressureAvg;
-Double pressureNow;
-Double pressureMax;
-Datetime pressureMaxDatetime;
-Double pressureMin;
-Datetime pressureMinDatetime;
-
-//Humidity
-Double humidityAvg;
-Double humidityNow;
-Double humidityMax;
-Datetime humidityMaxDatetime;
-Double humidityMin;
-Datetime humidityMinDatetime;
-
-	public static void main(String a[])
-	{
-		/*Format for sending av data:
-		main;dateTime:
-		temp;min;max;avg;timeMin;timeMax:
-		wind;min;max;avg;timeMin;timeMax;maxDirection:
-		pressure;min;max;avg;timeMin;timeMax:
-		humidity;min;max;avg;timeMin;timeMax
-		
-		Tidsformat: "YYYY-MM-DD HH:MM:SS"*/
-		splitString("");
-		makeMainEntry();
-		makeTempEntry();
-		makeWindEntry();
-		makePressureEntry();
-		makeHumidityEntry();
-	}
-	public void makeMainEntry(){
-		Db mainEntry = new Db("INSERT INTO logId (datetime) VALUES ("+mainDatetime+")");
-		entrykey=mainEntry.getKey();
-	}
+    public static void main(String a[]) {
+        jsonParse();
+        makeMainEntry();
+        makeTempEntry();
+        makeHumidityEntry();
+        makePressureEntry();
+        makeWindEntry();
+    }
+    public static void jsonParse(){
+        json = (JSONObject) JSONSerializer.toJSON(jsonString);
+        mainDatetime = convD(json.getString("mainDatetime"));
+    }
+    public static String convD(String datetime){
+        return datetime.replaceAll("\\W", "");
+    }
+    public static void makeMainEntry() {
+        Db mainEntry = new Db("INSERT INTO logId (datetime) VALUES ("+mainDatetime+")");
+        entryKey = mainEntry.getKey();
+    }
+    public static void makeTempEntry(){
+        JSONObject temp = json.getJSONObject("temp");
+        Double avg= temp.getDouble("avg");
+        Double now = temp.getDouble("now");
+        Double max = temp.getDouble("max");
+        Double min = temp.getDouble("min");
+        String timeMax = convD(temp.getString("timeMax"));
+        String timeMin = convD(temp.getString("timeMin"));
+        new Db("INSERT INTO temperature (`logId_id`, `temperatureAvg`, `temperatureNow`, `temperatureMax`, `timeMax`, `temperatureMin`, `timeMin`) VALUES ("+entryKey+", "+avg+", "+now+", "+max+", "+timeMax+", "+min+", "+timeMin+");");
+    }
+    public static void makeHumidityEntry(){
+        JSONObject humidity = json.getJSONObject("humidity");
+        Double avg= humidity.getDouble("avg");
+        Double now = humidity.getDouble("now");
+        Double max = humidity.getDouble("max");
+        Double min = humidity.getDouble("min");
+        String timeMax = convD(humidity.getString("timeMax"));
+        String timeMin = convD(humidity.getString("timeMin"));
+        new Db("INSERT INTO humidity (`logId_id`, `humidityAvg`, `humidityNow`, `humidityMax`, `timeMax`, `humidityMin`, `timeMin`) VALUES ("+entryKey+", "+avg+", "+now+", "+max+", "+timeMax+", "+min+", "+timeMin+");");
+    }
+    public static void makePressureEntry(){
+        JSONObject pressure = json.getJSONObject("pressure");
+        Double avg= pressure.getDouble("avg");
+        Double now = pressure.getDouble("now");
+        Double max = pressure.getDouble("max");
+        Double min = pressure.getDouble("min");
+        String timeMax = convD(pressure.getString("timeMax"));
+        String timeMin = convD(pressure.getString("timeMin"));
+        new Db("INSERT INTO pressure (`logId_id`, `pressureAvg`, `pressureNow`, `pressureMax`, `timeMax`, `pressureMin`, `timeMin`) VALUES ("+entryKey+", "+avg+", "+now+", "+max+", "+timeMax+", "+min+", "+timeMin+");");
+    }
+    public static void makeWindEntry(){
+        JSONObject wind = json.getJSONObject("wind");
+        Double avg= wind.getDouble("avg");
+        Double now = wind.getDouble("now");
+        Double max = wind.getDouble("max");
+        Double min = wind.getDouble("min");
+        Double maxDir = wind.getDouble("maxDir");
+        String timeMax = convD(wind.getString("timeMax"));
+        String timeMin = convD(wind.getString("timeMin"));
+        new Db("INSERT INTO wind (`logId_id`, `windspeedAvg`, `windspeedNow`, `windspeedMax`, `winddirectionMax`, `timeMax`, `windspeedMin`, `timeMin`) VALUES ("+entryKey+", "+avg+", "+now+", "+max+","+maxDir+", "+timeMax+", "+min+", "+timeMin+");");
+    }
 }
