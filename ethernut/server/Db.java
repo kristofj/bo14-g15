5c0ba63edae36b6a31cfb90f0e154b3645da6159
Leakim1991 <mikael-91@hotmail.com>
2014-02-28 21:09:31 +0100

    formatering

--- ethernut/server/Db.java
+++ ethernut/server/Db.java
@@ -1,48 +1,49 @@
 import java.sql.*;
 
 public class Db {
-	static final String DB_URL = "jdbc:mysql://j3nsen.com/hessdalen_weather";  
-	static final String USER = "xxxxxxxxxx";
-	static final String PASS = "xxxxxxxxxx";
-	int key=0;
-   
-	public Db(String sql) {
-		Connection conn = null;
-		Statement stmt = null;
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-			stmt = conn.prepareStatement(sql);
-			stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-			ResultSet rs = stmt.getGeneratedKeys();
-			rs = stmt.getGeneratedKeys();
-			if (rs.next()) {
-				key = rs.getInt(1);
-			} else {
-			}
-			rs.close();
-			stmt.close();
-			conn.close();
-            System.out.println("Executed: "+sql);
-			}catch(SQLException se){
-			se.printStackTrace();
-			}catch(Exception e){
-			e.printStackTrace();
-			}finally{
-			try{
-				if(stmt!=null)
-				stmt.close();
-			}catch(SQLException se2){
-			}
-			try{
-				if(conn!=null)
-				conn.close();
-			}catch(SQLException se){
-				se.printStackTrace();
-			}
-		}
-	}
-	public int getKey(){
-		return key;
-	}
+    static final String DB_URL = "jdbc:mysql://j3nsen.com/hessdalen_weather";
+    static final String USER = "hessdalen";
+    static final String PASS = "hessdalen123";
+    int key = 0;
+
+    public Db(String sql) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.prepareStatement(sql);
+            stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                key = rs.getInt(1);
+            } else {
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+            System.out.println("Executed: " + sql);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    public int getKey() {
+        return key;
+    }
 }
