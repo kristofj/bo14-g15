4e530c2657e6819b3c2afce89013579717cdca49
Leakim1991 <mikael-91@hotmail.com>
2014-03-22 05:50:35 +0100

    lagt til animasjon. må kvitte meg med sync call

--- datapresentasjon/dbToJson.php
+++ datapresentasjon/dbToJson.php
@@ -2,15 +2,19 @@
 mysql_connect("j3nsen.com", "hessdalen", "hessdalen123") or die(mysql_error());
 mysql_select_db("hessdalen_weather") or die(mysql_error());
 if($_GET["type"]=="temp"){
-    $data = mysql_query("select*from logId, temperature where logId.id=temperature.logId_id;")
+    $data = mysql_query("SELECT * FROM logId LEFT JOIN temperature ON logId.id=temperature.logId_id;")
     or die(mysql_error());
 }
 elseif($_GET["type"]=="humidity"){
-    $data = mysql_query("select*from logId, humidity where logId.id=humidity.logId_id;")
+    $data = mysql_query("SELECT * FROM logId LEFT JOIN humidity ON logId.id=humidity.logId_id;")
     or die(mysql_error());
 }
 elseif($_GET["type"]=="pressure"){
-    $data = mysql_query("select*from logId, pressure where logId.id=pressure.logId_id;")
+    $data = mysql_query("SELECT * FROM logId LEFT JOIN pressure ON logId.id=pressure.logId_id;")
+    or die(mysql_error());
+}
+elseif($_GET["type"]=="wind"){
+    $data = mysql_query("SELECT * FROM logId LEFT JOIN wind ON logId.id=wind.logId_id;")
     or die(mysql_error());
 }
 else{
@@ -19,17 +23,33 @@ else{
 $weatherData=array();
 while($row = mysql_fetch_array( $data ))
 {
-    $a = array(
-        'logId_id' => $row[0],
-        'time' => $row[1],
-        'station' => $row[2],
-        'valueAvg' => $row[4],
-        'valueNow' => $row[5],
-        'valueMax' => $row[6],
-        'timeMax' => $row[7],
-        'valueMin' => $row[8],
-        'timeMin' => $row[9],
-    );
+    if($_GET["type"]=="wind"){
+        $a = array(
+            'logId_id' => $row[0],
+            'time' => $row[1],
+            'station' => $row[2],
+            'valueAvg' => $row[4],
+            'valueNow' => $row[5],
+            'valueMax' => $row[6],
+            'dirMax' => $row[7],
+            'timeMax' => $row[8],
+            'valueMin' => $row[9],
+            'timeMin' => $row[10],
+        );
+    }
+    else{
+        $a = array(
+            'logId_id' => $row[0],
+            'time' => $row[1],
+            'station' => $row[2],
+            'valueAvg' => $row[4],
+            'valueNow' => $row[5],
+            'valueMax' => $row[6],
+            'timeMax' => $row[7],
+            'valueMin' => $row[8],
+            'timeMin' => $row[9],
+        );
+    }
     array_push($weatherData,$a);
 }
 echo json_encode($weatherData);
