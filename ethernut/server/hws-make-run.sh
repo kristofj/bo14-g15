035508e5c11f5d97f9a547468d0d2efed8c742fa
kristofj <kristoffer.jensen@hiof.no>
2014-05-20 20:09:06 +0200

    blabla

--- ethernut/server/hws-make-run.sh
+++ ethernut/server/hws-make-run.sh
@@ -2,13 +2,12 @@
 #hws-make-run.sh
 #Passer på at HessdalenWeatherServer kjører.
 
-program=HessdalenWeatherServer
+program=hessdalen16.jar
 
-if ps aux | grep -v grep | grep $program
-	then
-		exit
-	else
-		java $program &
-	fi
+if ps aux | grep -v grep | grep $program > /dev/null
+then
+	exit
+else
+        java -jar $program &
+fi
 exit
-
